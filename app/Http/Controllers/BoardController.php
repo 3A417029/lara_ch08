8cbda6a753434d06dc61a3c087fa259f5ceba77b
3A417029 <howardzhu0709@outlook.com>
2018-11-16 18:50:36 +0800

    練習四：DB存取 & Model 練習;修改BoardController 及 board.blade.php

--- app/Http/Controllers/BoardController.php
+++ app/Http/Controllers/BoardController.php
@@ -5,12 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Route;
 use View;
+use App\Score;
 
 class BoardController extends Controller
 {
     //
     public function getIndex()
     {
-        return View::make('board');
+        //return View::make('board');
+        $scores = Score::orderByTotal()
+        -> orderBySubject() -> get();
+        $data = ['scores' => $scores];
+        return view('board', $data);
     }
 }
